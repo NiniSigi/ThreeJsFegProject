608a3b2a81b672492c2ed9dc56dc0db35660bae2
ninos <nino.siegrist@netcetera.com>
2024-10-30 22:48:22 +0100

    Add Feg

--- TestForFeg/src/models/Experience.jsx
+++ TestForFeg/src/models/Experience.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { OrbitControls, ScrollControls } from "@react-three/drei";
 import { Tokio } from "./Tokio";
 import Pointer from "./Pointer";
+import { Feg } from "./Feg";
 
 const Experience = () => {
   return (
@@ -10,7 +11,7 @@ const Experience = () => {
       <OrbitControls />
       <Pointer position={[4, 0, 0]} />
       <Pointer position={[1, 0, 5]} />
-      <Tokio />
+      <Feg />
     </>
   );
 };
